7c0254a3b98861270e593e43f787e5094efc4ed2
noboomta <puvana.jp36@gmail.com>
2021-02-16 03:18:40 +0700

    add path and route to auth

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -8,32 +9,70 @@ import math
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_group09:fvgwpw72@158.108.182.0:2255/exceed_group09'
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 mongo = PyMongo(app)
 myTemp = mongo.db.temp
 myUser = mongo.db.user
+mySn = mongo.db.serial_number
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/login_post', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    flit = {'email' : email}
+    data = myUser.find_one(flit)
+    if not data:
+        flash('Please check your login details and try again.')
+        return redirect(url_for('login'))
+    if not check_password_hash(data["password"],password):
+        flash("Wrong password!!!!!!")
+        return redirect(url_for('login'))
+    sn = data["sn"]
+    return redirect(url_for("info", sn=sn))
+    
+
+@app.route('/info')
+def info():
+    sn = request.args.get('sn')
+    flit = {'sn' : sn}
+    data = myUser.find_one(flit)
+    data_temp = myTemp.find_one(flit)
+    return render_template('info.html', sn=sn, data=data, data_temp=data_temp)
+
+
 @app.route('/graph')
 def graph():
     return render_template('graph.html', name='nameeee')
+
 
 @app.route('/new_temp', methods=['POST'])
 def new_temp():
     data2 = request.json
     sn = data2['sn']
     temp = data2['temp']
+
     h = datetime.datetime.now().hour
+
     data = myTemp.find_one({'sn': sn})
+
     data['daily_temp'].append({"temp": temp, "hour": h})
     myTemp.replace_one(
         myTemp.find_one({'sn': sn}),
         data
     )
     return {"result": "add new temp done"}
-    # data ขึ้น mongo
+
 
 @app.route('/convert_avg', methods=["POST"])
 def convert_avg():
@@ -41,16 +80,17 @@ def convert_avg():
     sn = data['sn']
     all_temp = 0
     all_hr = 0
+
     daily = myTemp.find_one({'sn': sn})
     d = datetime.datetime.now().day
     m = datetime.datetime.now().month
     tim = str(d)+"-"+str(m)
-    # print(daily_temp)
-    # return "daily_temp"
+
     for ele in daily['daily_temp']:
         all_temp += ele['temp']
         all_hr += 1
-    avg = all_temp/all_hr
+
+    avg = 0 if all_hr==0 else all_temp/all_hr
     daily['daily_avg'].append({"temp_avg": avg, "day_month": tim})
     daily['daily_temp'] = []
     myTemp.replace_one(
@@ -59,9 +99,6 @@ def convert_avg():
     )
     return {"all_temp": all_temp, "all_hr": all_hr, "avg": avg}
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -74,34 +111,55 @@ def test():
     myUser.insert_one(data)
     return {"data": "done add test"}
 
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 @app.route('/signup_post', methods=['POST'])
 def signup_post():
     sn = request.form.get('sn')
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+    blood_type = request.form.get('blood-type')
+    job = request.form.get('job')
 
-    # if sn not in db:
-    #     flash('No sn')
-    #     return redirect(url_for('signup'))
+    dataSN = mySn.find_one({'sn': sn})
+    
+    if not dataSN:
+        flash('This serial number is not exist!')
+        return redirect(url_for('signup'))
+    if dataSN['signed_up']:
+        flash('This serial number is already signed up!')
+        return redirect(url_for('login'))
 
-    # if sn already exist:
-    #     flash('Email address already exists')
-    #     return redirect(url_for('signup'))
+    dataEmail = myUser.find_one({'email' : email})
 
-    # if sn not in db:
-    #     return redirect(url_for('signup'))
+    if dataEmail:
+        flash('This email is already signed up!')
+        return redirect(url_for('sign_up'))
 
     data = {
         "sn": sn,
         "email": email,
         "name": name,
-        "password": generate_password_hash(password, method='sha256')
+        "password": generate_password_hash(password, method='sha256'),
+        "blood_type": blood_type,
+        "job": job
     }
 
     myUser.insert_one(data)
-    
-    
+
+    return redirect(url_for('login'))
+
+@app.route('/get_temp', methods=['GET'])
+def get_temp():
+    data = request.json
+    sn = data['sn']
+    temp = myTemp.find_one({'sn': sn})
+    return temp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
