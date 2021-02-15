0877f5ef16e9d8350beda576c202c403ab847211
noboomta <puvana.jp36@gmail.com>
2021-02-16 01:42:32 +0700

    add 2method

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import datetime
 import math
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://exceed_user:1q2w3e4r@158.108.182.0:2277/exceed_backend'
+app.config['MONGO_URI'] = 'mongodb://exceed_group09:fvgwpw72@158.108.182.0:2255/exceed_group09'
 mongo = PyMongo(app)
-myCollection = mongo.db.g9
+myTemp = mongo.db.temp
+myUser = mongo.db.user
 
 @app.route('/')
 def index():
@@ -21,13 +22,57 @@ def graph():
 
 @app.route('/new_temp', methods=['POST'])
 def new_temp():
+    data2 = request.json
+    sn = data2['sn']
+    temp = data2['temp']
+    h = datetime.datetime.now().hour
+    data = myTemp.find_one({'sn': sn})
+    data['daily_temp'].append({"temp": temp, "hour": h})
+    myTemp.replace_one(
+        myTemp.find_one({'sn': sn}),
+        data
+    )
+    return {"result": "add new temp done"}
+    # data ขึ้น mongo
+
+@app.route('/convert_avg', methods=["POST"])
+def convert_avg():
     data = request.json
     sn = data['sn']
-    # data ขึ้น mongo
+    all_temp = 0
+    all_hr = 0
+    daily = myTemp.find_one({'sn': sn})
+    d = datetime.datetime.now().day
+    m = datetime.datetime.now().month
+    tim = str(d)+"-"+str(m)
+    # print(daily_temp)
+    # return "daily_temp"
+    for ele in daily['daily_temp']:
+        all_temp += ele['temp']
+        all_hr += 1
+    avg = all_temp/all_hr
+    daily['daily_avg'].append({"temp_avg": avg, "day_month": tim})
+    daily['daily_temp'] = []
+    myTemp.replace_one(
+        myTemp.find_one({'sn': sn}),
+        daily
+    )
+    return {"all_temp": all_temp, "all_hr": all_hr, "avg": avg}
 
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@app.route('/test', methods=['GET'])
+def test():
+    data = {
+        "sn": "sample sn",
+        "email": "sample email",
+        "name": "sample name",
+        "password": generate_password_hash("sample password", method='sha256')
+    }
+    myUser.insert_one(data)
+    return {"data": "done add test"}
 
 @app.route('/signup_post', methods=['POST'])
 def signup_post():
@@ -54,8 +99,8 @@ def signup_post():
         "password": generate_password_hash(password, method='sha256')
     }
 
-    myCollection.insert_one(data)
-    # return login
+    myUser.insert_one(data)
+    
     
 
 if __name__ == "__main__":
