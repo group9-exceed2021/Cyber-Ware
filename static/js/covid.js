2bce706e5a586daa0f7489a5a09dca18565023f7
SirapopKunjiak <sirapop.ku@ku.th>
2021-02-20 15:03:27 +0700

    update value and fetch to show in covid.html

--- static/js/covid.js
+++ static/js/covid.js
@@ -1,15 +1,31 @@
 async function get_th() {
     var data;
-    var Confirmed
+    var Confirmed, Recovered, Hospitalized, Deaths;
+    var NewConfirmed, NewRecovered, NewHospitalized, NewDeaths;
+    var UpdateDate;
     await fetch("http://185844a92011.ngrok.io/get_th_stat", requestOptions)
         .then(response => response.json())
         .then(json => {
             Confirmed = json.Confirmed
             document.getElementById("Confirmed Value").innerHTML = Confirmed
+            Recovered = json.Recovered
+            document.getElementById("Recovered Value").innerHTML = Recovered
+            Hospitalized = json.Hospitalized
+            document.getElementById("Hospitalized Value").innerHTML = Hospitalized
+            Deaths = json.Deaths
+            document.getElementById("Deaths Value").innerHTML = Deaths
+            NewConfirmed = json.NewConfirmed
+            document.getElementById("New Confirmed Value").innerHTML = NewConfirmed
+            NewRecovered = json.NewRecovered
+            document.getElementById("New Recovered Value").innerHTML = NewRecovered
+            NewHospitalized = json.NewHospitalized
+            document.getElementById("New Hospitalized Value").innerHTML = NewHospitalized
+            NewDeaths = json.NewDeaths
+            document.getElementById("New Deaths Value").innerHTML = NewDeaths
+            UpdateDate = json.UpdateDate
+            document.getElementById("Update Date Value").innerHTML = UpdateDate
         })
         .catch(error => console.log('error', error));
-    let h11 = document.getElementById("h1top");
-    h11.innerHTML = data;
 }
 var requestOptions = {
     method: 'GET',
