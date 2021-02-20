08f063c6a2495a3de2df2a4232bd7bbdcb1b512e
noboomta <puvana.jp36@gmail.com>
2021-02-20 16:52:25 +0700

    add many function

--- main.py
+++ main.py
@@ -38,52 +38,44 @@ def index():
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-
     flit = {'email' : email}
     data = myUser.find_one(flit)
     if not data:
         return {"result" : "check your login details and try again"}
     if not check_password_hash(data["password"],password):
         return {"result" : "Wrong password"}
-    return {"result" : "login sucessfully"}
+    return {"result" : "login successfully"}
+
 
 @app.route('/get_info',methods = ["GET"])
 @cross_origin()
 def get_info():
-    sn = request.args.get('sn')
-    flit = {'sn' : sn}
+    email = request.args.get('email')
+    flit = {'email' : email}
     data = myUser.find_one(flit)
-    data_temp = myTemp.find_one(flit)
+    sn = data["sn"]
+    filt2 = {"sn": sn}
+    data_temp = myTemp.find_one(filt2)
+    # datatest = {
+    #     "sn": data['sn'],
+    #     "email": data['email'],
+    #     "firstname": data['firstname'],
+    #     "surname": data['surname'],
+    #     "blood_type": data['blood_type'],
+    #     "job": data['job']
+    # }
     return {
-        "sn" : sn,
-        "info" : data,
-        "temp" : data_temp
+        "sn": data['sn'],
+        "email": data['email'],
+        "firstname": data['firstname'],
+        "surname": data['surname'],
+        "blood_type": data['blood_type'],
+        "job": data['job'],
+        "show_temp" : data_temp["daily_temp"][len(data_temp["daily_temp"])-1]['temp'],
+        "daily_temp" : data_temp['daily_temp'],
+        "avg": data_temp['daily_avg']
     }
-
-def convert_avg(sn):
-    all_temp = 0
-    all_hr = 0
-
-    daily = myTemp.find_one({'sn': sn})
-    d = datetime.datetime.now().day
-    m = datetime.datetime.now().month
-    tim = str(d)+"-"+str(m)
-
-    for ele in daily['daily_temp']:
-        all_temp += ele['temp']
-        all_hr += 1
-
-    avg = 0 if all_hr==0 else all_temp/all_hr
-    if len(daily['daily_avg']) == 14:
-        daily['daily_avg'].pop(0)
-    daily['daily_avg'].append({"temp_avg": avg, "day_month": tim})
-    daily['daily_temp'] = []
-    myTemp.replace_one(
-        myTemp.find_one({'sn': sn}),
-        daily
-    )
-    # return {"result": "Successful"}
-
+    # return {"fdd": "ghcdg"}
 
 @app.route('/new_temp', methods=['POST'])
 @cross_origin()
@@ -91,7 +83,7 @@ def new_temp():
     data2 = request.json
     sn = data2['sn']
     temp = data2['temp']
-    
+
     tz = pytz.timezone('Asia/Bangkok')
 
     m = datetime.datetime.now(tz).month
@@ -99,10 +91,27 @@ def new_temp():
     h = datetime.datetime.now(tz).hour
 
     data = myTemp.find_one({'sn': sn})
-    thelast = data['daily_temp'][len(data['daily_temp'])-1]
-    if thelast['day'] != d:
-        convert_avg(sn)
-
+    if len(data['daily_temp']) != 0:
+        thelast = data['daily_temp'][len(data['daily_temp'])-1]
+        if thelast['day'] != d:
+            all_temp = 0
+            all_hr = 0
+            daily = myTemp.find_one({'sn': sn})
+            tim = str(thelast["day"])+"-"+str(thelast["month"])
+            for ele in daily['daily_temp']:
+                all_temp += ele['temp']
+                all_hr += 1
+            avg = 0 if all_hr==0 else all_temp/all_hr
+            print(avg)
+            if len(daily['daily_avg']) == 14:
+                daily['daily_avg'].pop(0)
+            daily['daily_avg'].append({"temp_avg": avg, "day_month": tim})
+            daily['daily_temp'] = []
+            myTemp.replace_one(
+                myTemp.find_one({'sn': sn}),
+                daily
+            )
+    data = myTemp.find_one({'sn': sn})
     data['daily_temp'].append({"temp": temp, "month" : m, "day": d, "hour": h})
     myTemp.replace_one(
         myTemp.find_one({'sn': sn}),
@@ -123,11 +132,6 @@ def test():
     myUser.insert_one(data)
     return {"data": "done add test"}
 
-# render is not allowed
-# @app.route('/signup')
-# def signup():
-#     return render_template('signup.html')
-
 
 @app.route('/signup_post', methods=['POST'])
 @cross_origin()
@@ -142,17 +146,14 @@ def signup_post():
     dataSN = mySn.find_one({'sn': sn})
     
     if not dataSN:
-        flash('This serial number is not exist!')
-        return redirect(url_for('signup'))
+        return {"result" : 'This serial number is not exist!'}
     if dataSN['signed_up']:
-        flash('This serial number is already signed up!')
-        return redirect(url_for('login'))
+        return {"result" : 'This serial number is already signed up!'}
 
     dataEmail = myUser.find_one({'email' : email})
 
     if dataEmail:
-        flash('This email is already signed up!')
-        return redirect(url_for('sign_up'))
+        return {"result" : 'This email is already signed up!'}
 
     data = {
         "sn": sn,
@@ -164,16 +165,12 @@ def signup_post():
     }
 
     myUser.insert_one(data)
-
-    return redirect(url_for('login'))
-
-@app.route('/get_temp', methods=['GET'])
-@cross_origin()
-def get_temp():
-    data = request.json
-    sn = data['sn']
-    temp = myTemp.find_one({'sn': sn})
-    return temp
+    myTemp.insert_one({
+        "sn": sn,
+        "daily_temp": [],
+        "daily_avg": []
+    })
+    return {"result" : "login successful"}
 
 @app.route('/get_th_stat', methods=['GET'])
 @cross_origin()
